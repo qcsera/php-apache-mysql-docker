8c09cf8b8766c45ece9b6ea71b94ffd26cb9e1ca
Kucsera Csaba <qcsera@gmail.com>
2021-05-13 15:17:02 +0200

    Initial commit with docker files

---
+++ php/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = 'db'; // service name form focker-compose.eio_symlink
+$user = 'devuser';
+$password = 'devpass';
+$db = 'test_db';
+
+$conn = new mysqli($host, $user, $password, $db);
+if($conn->connect_error)
+{
+  echo 'connection failed' . $conn->connect_error;
+}
+echo 'Sucessfully connected to MYSQL';
+
+echo 'Hello from Docker';
+
+?>
